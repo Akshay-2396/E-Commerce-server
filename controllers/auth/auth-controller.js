4cbef2acf5a4ab47b972ab40cf63fbb503b9f6b4
Akshay-2396 <itsakshay.ak96@gmail.com>
2025-08-20 12:53:40 +0530

    modified

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -115,29 +115,7 @@ const logoutUser = (req, res) => {
 /**
  * AUTH MIDDLEWARE
  */
-// const authMiddleware = (req, res, next) => {
-//   const token =
-//     req.cookies.token || req.header("Authorization")?.replace("Bearer ", "");
 
-//   if (!token) {
-//     return res.status(401).json({
-//       success: false,
-//       message: "Unauthorized! No token provided",
-//     });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = decoded;
-//     next();
-//   } catch (error) {
-//     console.error("Auth Middleware Error:", error);
-//     return res.status(401).json({
-//       success: false,
-//       message: "Invalid or expired token",
-//     });
-//   }
-// };
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.header("Authorization");
